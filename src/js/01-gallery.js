cf5333cec6ae2f77ff3f1c49b2851a21ebcfd2f6
engelisart <144841027+engelisart@users.noreply.github.com>
2023-11-06 20:08:25 +0100

    updated 1hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items.js";
+import SimpleLightbox from 'simplelightbox'
+import 'simplelightbox/dist/simple-lightbox.min.css'
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((galleryItem) => {
+  let item = `<li class="gallery__item">
+    <a class="gallery__link" href="${galleryItem.original}">
+       <img 
+       class="gallery__image" 
+       src="${galleryItem.preview}" 
+       alt="${galleryItem.description}" />
+    </a>
+ </li>`;
+
+  gallery.innerHTML += item;
+});
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
