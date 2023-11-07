5ef02aaadcb2563654cb544d522427dcf105b8fc
engelisart <144841027+engelisart@users.noreply.github.com>
2023-11-07 21:06:54 +0100

    updated hw2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
+
+const iframeElemVideo = document.querySelector('#vimeo-player');
+
+const player = new Player(iframeElemVideo, {
+  id: 19231868,
+  width: 640,
+});
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(event) {
+     
+    localStorage.setItem("videoplayer-current-time", event.seconds)
+}
+
+player.on('loaded',async(event)=>{
+    secs = localStorage.getItem('videoplayer-current-time')
+    try{
+
+        await player.setCurrentTime(secs)
+    }
+    catch(e){
+        console.log(e)
+    }
+
+})
+
+
+
+
 
